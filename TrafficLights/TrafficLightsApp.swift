21a12d77beea1adbef8ac31cf8d4ab4062f5c502
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-09-06 22:11:43 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by slava on 06.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
